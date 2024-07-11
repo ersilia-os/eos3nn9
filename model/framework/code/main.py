4c418cfd739932904c0e54bb5cd8432cb5154350
HarmonySosa <hsosa2406@gmail.com>
2024-07-11 12:17:46 -0700

    working on updating format to run model

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -2,8 +2,16 @@
 import os
 import csv
 import sys
+import pandas as pd
+import subprocess
+import base64
+import pickle
+import numpy as np
 from rdkit import Chem
 from rdkit.Chem.Descriptors import MolWt
+
+np.bool = np.bool_ # added for attribute error
+np.object = np.object_
 
 # parse arguments
 input_file = sys.argv[1]
@@ -17,16 +25,69 @@ def my_model(smiles_list):
     return [MolWt(Chem.MolFromSmiles(smi)) for smi in smiles_list]
 
 
+# Molecular descriptor calculator option
+def desc_calc():
+    # Performs the descriptor calculation
+    bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/MACCSFingerprinter.xml -dir ./ -file descriptors_output.csv"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+    # Read the original input file to get the order
+    with open("molecule.smi", "r") as f:
+        input_order = [line.split()[1] for line in f]
+
+    # Read the output CSV
+    descriptors_df = pd.read_csv("descriptors_output.csv")
+
+    # Sort the output based on the input order
+    sorted_descriptors_df = descriptors_df.set_index('Name').reindex(input_order).reset_index()
+    
+    # Save the sorted output to a new file
+    sorted_descriptors_df.to_csv("sorted_descriptors_output.csv", index=False)
+
+# File download option
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="prediction.csv">Download Predictions</a>'
+    return href
+
+# Model building section
+def build_model(input_data, load_data):
+    load_model = pickle.load(open('Mpro_model.pkl', 'rb'))
+    # Apply model to make predictions
+    prediction = load_model.predict(input_data)
+    prediction_output = pd.Series(prediction, name='pIC50')
+    molecule_name = pd.Series(load_data[1], name='molecule_name')
+    df = pd.concat([molecule_name, prediction_output], axis=1)
+    print(df) # display output
+
+
+
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
     reader = csv.reader(f)
     next(reader)  # skip header
     smiles_list = [r[0] for r in reader]
 
+# write molecule.smi with tabs instead of spaces for Padel functionality
+output_file = "molecule.smi"
+with open(output_file, "w") as exportFile:
+    for line in smiles_list:
+        new_line = line.replace(' ', '\t')
+        exportFile.write(new_line + '\n')
+
+load_data = pd.read_table(output_file, sep='\t', header=None)
+
 # run model
+desc_calc()
+desc = pd.read_csv('sorted_descriptors_output.csv') # read new sorted output instead of randomly sorted descriptors_output
+Xlist = list(pd.read_csv('lists_of_descriptor.csv').columns)
+desc_subset = desc[Xlist]
+build_model(desc_subset, load_data)
 outputs = my_model(smiles_list)
 
-#check input and output have the same lenght
+#check input and output have the same length
 input_len = len(smiles_list)
 output_len = len(outputs)
 assert input_len == output_len
